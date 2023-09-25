d8143dc84b7fba451049c67d50afcc6e17966fa3
Nam Nguyen <nam.n.nguyen247@gmail.com>
2023-09-25 09:52:19 -0500

    add OpenZeppelin smart contract

--- common/math.go
+++ common/math.go
@@ -12,3 +12,13 @@ func BigPow(x, y int64) *big.Int {
 func Exp(base, exponent *big.Int) *big.Int {
 	return math.Exp(base, exponent)
 }
+
+// DivideBigInt performs division of two big.Int numbers and returns the result as a float64.
+// If an error occurs during the conversion, it returns 0 and the error.
+func DivideBigInt(dividend *big.Int, divisor *big.Int) float64 {
+	dividendFloat := new(big.Float).SetInt(dividend)
+	divisorFloat := new(big.Float).SetInt(divisor)
+	quotient := new(big.Float).Quo(dividendFloat, divisorFloat)
+	quotientFloat, _ := quotient.Float64()
+	return quotientFloat
+}
